2a100cef3f5750d1948c2b5a1d256ecb76cf413c
YvNathan <ratefinjanaharynathan@gmail.com>
2026-06-12 10:04:33 +0300

    feat: init request processing servlet

---
+++ src/main/java/itu/myframework/RequestControllerServlet.java
@@ -0,0 +1,27 @@
+package itu.myframework;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class RequestControllerServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequest(req, res);
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequest(req, res);
+    }
+
+    private void processRequest(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/plain");
+        PrintWriter printer = res.getWriter();
+
+        printer.print("Ca marche");
+    }
+
+}
